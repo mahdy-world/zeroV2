15e726625dcaaf6b7c68d59b645afe63c60f9d7a
dawnbergdev <mmos7239@gmail.com>
2022-02-09 20:44:31 +0200

    done

--- ZerOne/settings.py
+++ ZerOne/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-as!gjsn87_y_!jp9*5j$^x1g)4h%nuy4uux!lb(1)9a8i5m)hl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
